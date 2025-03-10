0cc2935648f82bc20cc34b7d3f10fd935559f6a3
Maxim Gusev <m.gusev@dodobrands.io>
2025-03-10 04:29:37 +0300

    D

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "Maxim Gusev SRE Blog",
+    pageTitleSuffix: "Publushed notes from obsidian",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
